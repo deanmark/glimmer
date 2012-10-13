60d2edd5926c07bb2bd67006acf49a3128a5db07
Dean Mark <dean.mark@gmail.com>
2012-10-13 19:49:59 +0200

    Added GPLv3 license

--- LevelPopulationSolver/OurSolver/LevelPopulationSolverOpticallyThin.m
+++ LevelPopulationSolver/OurSolver/LevelPopulationSolverOpticallyThin.m
@@ -1,3 +1,28 @@
+%{
+GLIMMER is a visual LVG (Large Velocity Gradient) analysis tool.
+
+Copyright (C) 2012  Dean Mark <deanmark at gmail>, 
+		Prof. Amiel Sternberg <amiel at wise.tau.ac.il>, 
+		Department of Astrophysics, Tel-Aviv University
+
+Documentation for the program is posted at http://deanmark.github.com/glimmer/
+
+This file is part of GLIMMER.
+
+GLIMMER is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GLIMMER is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 classdef LevelPopulationSolverOpticallyThin < handle
     
     properties(SetAccess = private)
